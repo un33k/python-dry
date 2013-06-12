866237c1c4eca54c663f44d2feb00355b042b188
Val Neekman <val@neekware.com>
2013-06-12 00:07:42 -0400

    add search utilities and test cases

--- pydry/string.py
+++ pydry/string.py
@@ -1,10 +1,42 @@
 import re
 
-def string_clean(string):
-    """ Clean text -- single space """
-    txt = re.sub("\r\n|\n|\r|\t", ' ', string)
-    txt = re.sub(" +", ' ', txt)
+def str_single_space(string):
+    """ Converts one or more spaces into a single space """
+    txt = re.sub(" +", ' ', string)
     return txt
+
+def str_single_line(string):
+    """ Converts a content with multiple line into a single line content """
+    txt = re.sub("\r\n|\n|\r|\t", ' ', string)
+    return txt
+
+def str_serialize_clean(string):
+    """ Converts content into a single line with single spaces """
+    txt = str_single_space(str_single_line(string))
+    return txt
+
+def str_find_between_regex(start, end, string, lazy=True, options=re.DOTALL|re.MULTILINE, all=False):
+    """ Returns substring btween two strings tags. all=(returns all matches), lazy=(return shortest match)"""
+    pattern = '(?<={}){}(?={})'.format(re.escape(start), '(.*?)' if lazy else '(.*)', re.escape(end))
+    match = re.findall(pattern, string, options)
+    if match:
+        if all:
+            return match
+        else:
+            return match[0]
+    return ''
+
+def str_find_between_search(start, end, string, reverse=False):
+    try:
+        if reverse:
+            start = string.rindex(start) + len(start)
+            end = string.rindex(end, start)
+        else:
+            start = string.index(start) + len(start)
+            end = string.index(end, start)
+        return string[start:end]
+    except ValueError:
+        return ""
 
 
 
--- test.py
+++ test.py
@@ -1,14 +1,64 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from pydry.string import string_clean
+from pydry.string import *
 
 class TestDry(unittest.TestCase):
 
-    def test_manager(self):
+    def test_single_space(self):
         string = "Hello\r\nWorld\t      You  Hoo"
-        clean_string = string_clean(string)
+        clean_string = str_single_space(string)
+        self.assertEquals(clean_string, "Hello\r\nWorld\t You Hoo")
+
+    def test_single_line(self):
+        string = "Hello\r\nWorld\t      You  Hoo"
+        clean_string = str_single_line(string)
+        self.assertEquals(clean_string, "Hello World       You  Hoo")
+
+    def test_serialize(self):
+        string = "Hello\r\nWorld\t      You  Hoo"
+        clean_string = str_serialize_clean(string)
         self.assertEquals(clean_string, "Hello World You Hoo")
+
+    def test_find_between_regex(self):
+        string = "Hello\r\nWorld\t      You  Hoo Hoo <a href='foo' title='some title'>You Hoo</a>"
+        start = "World\t"
+        end = '<'
+        # find substring the lazy way (shortest substring)
+        substring = str_find_between_regex(start, end, string)
+        self.assertEquals(substring, "      You  Hoo Hoo ")
+        
+        # find substring the greedy way (longest substring)
+        substring = str_find_between_regex(start, end, string, lazy=False)
+        self.assertEquals(substring, "      You  Hoo Hoo <a href='foo' title='some title'>You Hoo")
+
+    def test_find_between_regex(self):
+        string = "Hello\r\nWorld\t      You  Hoo Hoo <a href='foo' title='some title'>You Hoo</a> Hello\r\nWorld\t      You  Hoo Hoo"
+        start = "World\t"
+        end = 'Hoo'
+        # find substring the lazy way (shortest substring)
+        substring = str_find_between_regex(start, end, string)
+        self.assertEquals(substring.strip(), "You")
+
+        # find substring the lazy way (shortest substring)
+        substrings = str_find_between_regex(start, end, string, all=True)
+        self.assertEquals(len(substrings), 2)
+        self.assertEquals(substrings[0].strip(), "You")
+        self.assertEquals(substrings[1].strip(), "You")
+
+    def test_find_between_search(self):
+        string = "Hello\r\nWorld Hoo\t      You  You Hoo <a href='foo' title='some title'>You You</a>"
+        start = "Hoo"
+        end = '<'
+        # find substring by searching from left to right. (matching the first "end")
+        substring = str_find_between_search(start, end, string)
+        self.assertEquals(substring, "\t      You  You Hoo ")
+        
+        # find substring by search from right to left. (matching the first "start")
+        substring = str_find_between_search(start, end, string, reverse=True)
+        self.assertEquals(substring, " <a href='foo' title='some title'>You You")
+
+
 
 if __name__ == '__main__':
     unittest.main()
